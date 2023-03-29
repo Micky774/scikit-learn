3faa339f344ca86a779d47ef567dc080d7cc67bd
Meekail Zain <zainmeekail@gmail.com>
2023-03-29 15:02:41 -0400

    Implemented unrolling and improved loop structure

--- sklearn/metrics/_dist_optim.c
+++ sklearn/metrics/_dist_optim.c
@@ -1,9 +1,6 @@
 #ifdef __SSE3__
     #define HAS_SIMD 1
-
-    #include <emmintrin.h>
-    #include <xmmintrin.h>
-    #include <pmmintrin.h>
+    #include <x86intrin.h>
 
     typedef __m128d simd_float64_t;
     typedef __m128 simd_float32_t;
@@ -13,30 +10,33 @@
         return _mm_andnot_ps(sign_mask, x);
     }
 
-    // NUMPY VARIANT
     inline simd_float64_t abs_pd(simd_float64_t x) {
-        // !sign_mask & x where sign_mask = -0. = 1 << 63
-        return _mm_and_pd(
-            x,
-            _mm_castsi128_pd(_mm_set1_epi64x(0x7fffffffffffffffLL))
-        );
+        const simd_float64_t sign_mask = _mm_set1_pd(-0.f); // -0.f = 1 << 63
+        return _mm_andnot_pd(sign_mask, x);
     }
 
     float simd_manhattan32(const float* x, const float* y, ssize_t t) {
-        simd_float32_t simd_x;
-        simd_float32_t simd_y;
-        ssize_t remainder = t % 4; // SIMD register can hold four floats
+        simd_float32_t simd_x_1, simd_x_2;
+        simd_float32_t simd_y_1, simd_y_2;
+        ssize_t loop_width = 8; // Two SIMD registers can hold eight floats
+        ssize_t remainder = t % loop_width;
         ssize_t n_iter = t - remainder;
         ssize_t idx;
 
-        simd_float32_t sum = _mm_setzero_ps();
-        for(int k = 0; k < t; k+=2) {
-            simd_x = _mm_set_ps(x[k], x[k+1], x[k+2], x[k+3]);
-            simd_y = _mm_set_ps(y[k], y[k+1], y[k+2], y[k+3]);
-            sum += abs_ps(_mm_sub_ps(simd_x, simd_y));
+        simd_float32_t sum_1 = _mm_setzero_ps();
+        simd_float32_t sum_2 = _mm_setzero_ps();
+        for(idx = 0; idx < n_iter; idx += loop_width) {
+            simd_x_1 = _mm_set_ps(x[idx], x[idx + 1], x[idx + 2], x[idx + 3]);
+            simd_y_1 = _mm_set_ps(y[idx], y[idx + 1], y[idx + 2], y[idx + 3]);
+            sum_1 += abs_ps(_mm_sub_ps(simd_x_1, simd_y_1));
+
+            simd_x_2 = _mm_set_ps(x[idx + 4], x[idx + 5], x[idx + 6], x[idx + 7]);
+            simd_y_2 = _mm_set_ps(y[idx + 4], y[idx + 5], y[idx + 6], y[idx + 7]);
+            sum_2 += abs_ps(_mm_sub_ps(simd_x_2, simd_y_2));
         }
 
-        simd_float32_t hsum = _mm_hadd_ps(sum, sum);
+        sum_1 = _mm_add_ps(sum_1, sum_2);
+        simd_float32_t hsum = _mm_hadd_ps(sum_1, sum_1);
         hsum = _mm_hadd_ps(hsum, hsum);
         float output_sum = *(float*)&hsum;
         for(idx = n_iter; idx < t; idx++){
@@ -47,19 +47,26 @@
     }
 
     double simd_manhattan(const double* x, const double* y, ssize_t t) {
-        simd_float64_t simd_x;
-        simd_float64_t simd_y;
-        ssize_t remainder = t % 2; // SIMD register can hold two doubles
+        simd_float64_t simd_x_1, simd_x_2;
+        simd_float64_t simd_y_1, simd_y_2;
+        ssize_t loop_width = 4; // Two SIMD registers can hold four doubles
+        ssize_t remainder = t % loop_width;
         ssize_t n_iter = t - remainder;
         ssize_t idx;
 
-        simd_float64_t sum = _mm_setzero_pd();
-        for(idx = 0; idx < n_iter; idx+=2) {
-            simd_x = _mm_set_pd(x[idx], x[idx+1]);
-            simd_y = _mm_set_pd(y[idx], y[idx+1]);
-            sum += abs_pd(_mm_sub_pd(simd_x, simd_y));
+        simd_float64_t sum_1 = _mm_setzero_pd();
+        simd_float64_t sum_2 = _mm_setzero_pd();
+        for(idx = 0; idx < n_iter; idx += loop_width) {
+            simd_x_1 = _mm_set_pd(x[idx], x[idx + 1]);
+            simd_y_1 = _mm_set_pd(y[idx], y[idx + 1]);
+            sum_1 += abs_pd(_mm_sub_pd(simd_x_1, simd_y_1));
+
+            simd_x_2 = _mm_set_pd(x[idx + 2], x[idx + 3]);
+            simd_y_2 = _mm_set_pd(y[idx + 2], y[idx + 3]);
+            sum_2 += abs_pd(_mm_sub_pd(simd_x_2, simd_y_2));
         }
-        simd_float64_t hsum = _mm_hadd_pd(sum, sum);
+        sum_1 = _mm_add_pd(sum_1, sum_2);
+        simd_float64_t hsum = _mm_hadd_pd(sum_1, sum_1);
         double output_sum = *(double*)&hsum;
         for(idx = n_iter; idx < t; idx++){
             output_sum += fabs(x[idx] - y[idx]);
